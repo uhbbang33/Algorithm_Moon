c344e16705bb45db317120499069131ac882e319
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2023-12-11 09:59:03 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -5,23 +5,15 @@ using namespace std;
 
 string solution(int a, int b) {
     string answer = "";
-    string date[7] = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+    string day[7] = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+    int date[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30};
     
     int num = b - 1;
     
-    if( a > 1) num += 31;
-    if(a > 2 )num += 29;
-    if(a > 3) num += 31;
-    if(a > 4) num += 30;
-    if(a > 5) num += 31;
-    if(a > 6) num += 30;
-    if(a > 7) num += 31;
-    if(a > 8) num += 31;
-    if(a > 9) num += 30;
-    if(a > 10) num += 31;
-    if(a > 11) num += 30;
+    for(int i= 0; i < a - 1; ++i)
+        num += date[i];
     
-    answer = date[num % 7];
+    answer = day[num % 7];
         
     return answer;
 }
