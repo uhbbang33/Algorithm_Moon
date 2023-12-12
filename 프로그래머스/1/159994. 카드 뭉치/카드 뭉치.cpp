3320371288f8391c8ad20a755fa235a3aa859d06
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2023-12-12 09:33:59 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "";
+    
+    int goalSize = goal.size();
+    
+    for(int i = 0; i < goalSize; ++i) {
+        if(goal[0] == cards1[0]){
+            goal.erase(goal.begin());
+            cards1.erase(cards1.begin());
+             
+        }
+        else if(goal[0] == cards2[0]){
+            goal.erase(goal.begin());
+            cards2.erase(cards2.begin());
+        }
+        else{
+            answer = "No";
+            continue;
+        }
+    }
+    if(answer == "")
+        answer = "Yes";
+        
+        
+    return answer;
+}
