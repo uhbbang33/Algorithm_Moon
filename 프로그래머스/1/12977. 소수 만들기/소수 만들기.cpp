1920f4111c42b9ac54d41de30e7db69de51d09bc
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2023-12-19 09:25:37 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool isPrimeNumber(int num){
+    for(int i = 2; i < num; ++i)
+        if((num % i) == 0)
+            return false;
+    return true;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+
+    for(int i = 0; i < nums.size(); ++i)
+        for(int j = i + 1; j < nums.size(); ++j)
+            for(int k = j + 1; k < nums.size(); ++k)
+                if(isPrimeNumber(nums[i] + nums[j] + nums[k]))
+                    ++answer;
+    return answer;
+}
+
