c86be755b04bca156dea099229e42860d61e6540
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-04-12 19:09:17 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.16 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cpp
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cpp
@@ -1,32 +1,28 @@
 #include <string>
 #include <vector>
-
+#include <map>
 using namespace std;
 
 vector<int> solution(vector<string> keymap, vector<string> targets) {
     vector<int> answer;
+    map<char, int> myMap;
     
-    for(int i = 0; i < targets.size(); ++i){
-        string curTarget = targets[i];
+    for(int i = 0; i<keymap.size(); ++i)
+        for(int j =0; j < keymap[i].length(); ++j){
+            if(myMap.find(keymap[i][j]) == myMap.end())
+                myMap.insert(make_pair(keymap[i][j], j + 1));
+            else if(myMap[keymap[i][j]] > j+ 1)
+               myMap[keymap[i][j]] = j+1;
+        }
+    
+    for(int i = 0; i<targets.size(); ++i){
         answer.push_back(0);
-        for(int j = 0; j < curTarget.length(); ++j){
-            int minNum = 101;
-            for(int k = 0; k < keymap.size(); ++k){
-                for(int l = 0; l < keymap[k].length(); ++l){
-                    if(keymap[k][l] == curTarget[j]){
-                        if(minNum > l)
-                            minNum = l+1;
-                        break;
-                    }
-                }
-            }
-            
-            if(minNum <= 100)
-                answer[i] += minNum;
-            else {
+        for(int j = 0; j < targets[i].length(); ++j){
+            if(myMap.find(targets[i][j]) == myMap.end()){
                 answer[i] = -1;
                 break;
             }
+            answer[i] += myMap[targets[i][j]];
         }
     }
     
