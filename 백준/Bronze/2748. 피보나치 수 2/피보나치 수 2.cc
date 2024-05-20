df5ca6ad031dca5ea736ae17a919e361f3d86eb2
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-05-20 13:17:13 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	vector<long long> v;
+	v.push_back(0);
+	v.push_back(1);
+
+	int n = 0;
+	cin >> n;
+	
+	for (int i = 2; i <= n; ++i)
+		v.push_back(v[i - 1] + v[i - 2]);
+
+	cout << v[n];
+
+	return 0;
+}
