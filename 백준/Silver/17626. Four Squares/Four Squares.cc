ed34d885e40ee38ba971e7385344209c98684e07
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-05-22 13:44:31 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.cc
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -2,6 +2,8 @@
 #include <vector>
 using namespace std;
 
+int result[50000]{};
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -9,11 +11,8 @@ int main() {
 	int n = 0;
 	cin >> n;
 
-	vector<int> result;
-	result.push_back(0);
-
 	for (int i = 1; i <= n; ++i) {
-		result.push_back(result[i - 1] + 1);
+		result[i] = result[i - 1] + 1;
 		for (int j = 1; j * j <= i; ++j)
 			result[i] = min(result[i], result[i - j * j] +1);
 	}
