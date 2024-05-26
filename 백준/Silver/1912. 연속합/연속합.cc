ed52bcf371c166f3c1a6ce243ab26f62b8c3d79a
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-05-26 14:57:03 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
@@ -10,28 +8,22 @@ int main() {
 	int n = 0;
 	cin >> n;
 	
-	vector<int> nums;
-	nums.push_back(0);
-	vector<int> dp;
-	dp.push_back(0);
-
+	int temp = 0;
 	int num;
 	int max = 0;
 	for (int i = 1; i <= n; ++i) {
 		cin >> num;
-		nums.push_back(num);
-		dp.push_back(0);
-
+		
 		if (i == 1)
-			max = dp[i] = num;
+			max = num;
 
-		if (nums[i] >= dp[i - 1] + nums[i])
-			dp[i] = nums[i];
+		if (num >= temp + num)
+			temp = num;
 		else
-			dp[i] = dp[i - 1] + nums[i];
+			temp = temp + num;
 
-		if (dp[i] > max)
-			max = dp[i];
+		if (temp > max)
+			max = temp;
 	}
 
 	cout << max;
