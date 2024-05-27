6fce269912c5be13d19c6b00a9fdffafac41877d
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-05-27 14:08:33 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dp[1000]{};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n = 0;
+    cin >> n;
+
+    int num = 0;
+    vector<int> v;
+    v.push_back(0);
+
+    int max = 0;
+    for (int i = 1; i <= n; ++i) {
+        cin >> num;
+        v.push_back(num);
+
+        dp[i] = num;
+
+        int tempMax = 0;
+        int tempLocation = 1;
+
+        for (int j = i - 1; j >= 1; --j) {
+            if (v[i] > v[j] && tempMax < dp[j]) {
+                tempMax = dp[j];
+                tempLocation = j;
+            }
+        }
+
+        dp[i] = tempMax + v[i];
+
+        if (max < dp[i]) {
+            max = dp[i];
+        }
+    }
+
+    cout << max;
+
+    return 0;
+}
