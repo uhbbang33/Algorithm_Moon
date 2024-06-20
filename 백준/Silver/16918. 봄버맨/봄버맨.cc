bc9e29963527200e0a0840979779a6d7e13807ca
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-06-20 17:35:11 +0900

    [Silver I] Title: 봄버맨, Time: 4 ms, Memory: 2104 KB -BaekjoonHub

---
+++ 백준/Silver/16918. 봄버맨/봄버맨.cc
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+char graph[201][201]{};
+char tempGraph[201][201]{};
+int r, c, n = 0;
+
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { -1, 1, 0, 0 };
+
+void BFS(int a, int b) {
+	queue<pair<int, int>> q;
+	q.push({ a, b });
+
+	tempGraph[a][b] = '.';
+
+	while (!q.empty()) {
+		int y = q.front().first;
+		int x = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; ++i) {
+			int ny = a + dy[i];
+			int nx = b + dx[i];
+
+			if (ny < 0 || nx < 0 || ny >= r || nx >= c)
+				continue;
+
+			if (graph[ny][nx] == 'O' && tempGraph[ny][nx] != '.')
+				q.push({ ny, nx });
+
+			tempGraph[ny][nx] = '.';
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> r >> c >> n;
+
+	for (int i = 0; i < r; ++i)
+		for (int j = 0; j < c; ++j) {
+			cin >> graph[i][j];
+			tempGraph[i][j] = 'O';
+		}
+
+	if (n == 1) {
+		for (int i = 0; i < r; ++i) {
+			for (int j = 0; j < c; ++j)
+				cout << graph[i][j];
+			cout << "\n";
+		}
+	}
+	else if (n % 2 == 0) {
+		for (int i = 0; i < r; ++i) {
+			for (int j = 0; j < c; ++j)
+				cout << "O";
+			cout << "\n";
+		}
+	}
+	else {
+		for (int i = 0; i < r; ++i)
+			for (int j = 0; j < c; ++j)
+				if (graph[i][j] == 'O')
+					BFS(i, j);
+
+		if (n % 4 == 1) {
+			for (int i = 0; i < r; ++i)
+				for (int j = 0; j < c; ++j) {
+					graph[i][j] = tempGraph[i][j];
+					tempGraph[i][j] = 'O';
+				}
+			for (int i = 0; i < r; ++i)
+				for (int j = 0; j < c; ++j)
+					if (graph[i][j] == 'O')
+						BFS(i, j);
+		}
+
+		for (int i = 0; i < r; ++i) {
+			for (int j = 0; j < c; ++j)
+				cout << tempGraph[i][j];
+			cout << "\n";
+		}
+	}
+
+	return 0;
+}
