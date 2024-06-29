d32202a3343e94d2435a56dbdca3613438402fec
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-06-29 16:09:49 +0900

    [Silver I] Title: 민겸 수, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/21314. 민겸 수/민겸 수.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string s;
+	cin >> s;
+
+	string maxStr = "";
+	string minStr = "";
+	
+	int mCount = 0;
+	for (int i = 0; i < s.length(); ++i) {
+		if (s[i] == 'M')
+			++mCount;
+		else if (s[i] == 'K') {
+			maxStr += "5";
+
+			if (mCount >= 1)
+			{
+				minStr += "1";
+				for (int i = 0; i < mCount; ++i) {
+					maxStr += "0";
+					if (i != mCount - 1)
+						minStr += "0";
+				}
+				mCount = 0;
+			}
+			minStr += "5";
+		}
+	}
+	if (mCount >= 1) {
+		minStr += "1";
+		for (int i = 0; i < mCount; ++i) {
+			if (i != mCount - 1)
+				minStr += "0";
+			maxStr += "1";
+		}
+	}
+
+	cout << maxStr << "\n";
+	cout << minStr;
+
+	return 0;
+}
