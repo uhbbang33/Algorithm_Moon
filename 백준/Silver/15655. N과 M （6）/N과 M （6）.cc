e6cf2ab1c2188b07da35f7c2072287c6553e267e
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-07-23 15:07:00 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+
+int arr[8]{};
+bool used[8]{};
+
+void Backtracking(int num) {
+	if (num == m) {
+		for (int i = 0; i < m; ++i)
+			cout << arr[i] << " ";
+		cout << "\n";
+		return;
+	}
+	
+	for (int i = 1; i <= n; ++i) {
+		if (!used[i]) {
+			if (num > 0 && arr[num - 1] > v[i-1])
+				continue;
+			arr[num] = v[i - 1];
+			used[i] = true;
+			Backtracking(num + 1);
+			used[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	v.resize(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i];
+	sort(v.begin(), v.end());
+
+	Backtracking(0);
+
+	return 0;
+}
