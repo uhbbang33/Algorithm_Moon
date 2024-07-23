c24cf486cbdb54f91be53348ff3412c86694ec03
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-07-23 14:47:53 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[8]{};
+
+void Backtracking(int num) {
+	if (num == m) {
+		for (int i = 0; i < m; ++i)
+			cout << arr[i] << " ";
+		cout << "\n";
+		return;
+	}
+	
+	for (int i = 1; i <= n; ++i) {
+		if (num > 0 && arr[num - 1] > i)
+			continue;
+		arr[num] = i;
+		Backtracking(num + 1);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+	
+	Backtracking(0);
+
+	return 0;
+}
