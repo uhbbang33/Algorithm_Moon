de6bfbac1870b2644102770e54b661462e6ab82f
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-07-30 12:59:52 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+pair<int, int> p[100001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n = 0;
+	cin >> n;
+	
+	for (int i = 0; i < n; ++i)
+		cin >> p[i].second >> p[i].first;
+	sort(p, p + n);
+
+	for (int i = 0; i < n; ++i)
+		cout << p[i].second << " " << p[i].first << "\n";
+
+	return 0;
+}
