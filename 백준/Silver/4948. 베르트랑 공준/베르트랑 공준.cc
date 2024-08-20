a6407155ccb1be3f7ed41493cded0e51f7deda1c
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-08-20 18:36:50 +0900

    [Silver II] Title: 베르트랑 공준, Time: 4 ms, Memory: 2260 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -1,28 +1,33 @@
 #include <iostream>
 using namespace std;
 
-bool IsPrime(int num) {
-	for (int i = 2; i * i <= num; ++i) {
-		if (num % i == 0)
-			return false;
-	}
-	return true;
-}
+#define MAX_N 123456 * 2
 
+bool isPrime[MAX_N + 1]{};
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
+	fill_n(isPrime, MAX_N + 1, true);
+
+	isPrime[0] = false;
+	isPrime[1] = false;
+
+	for (int i = 2; i * i <= MAX_N; ++i)
+		for (int j = i * i; j <= MAX_N; j += i) 
+			isPrime[j] = false;
+
 	int n = -1;
 	while (1) {
 		cin >> n;
+
 		if (n == 0)
 			break;
 
 		int cnt = 0;
 		for (int i = n + 1; i <= n * 2; ++i)
-			if (IsPrime(i))
+			if (isPrime[i])
 				++cnt;
 
 		cout << cnt << "\n";
