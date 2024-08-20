f19596ff8df9a48d0b4e9040b0d873e505ac75e4
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-08-20 14:56:53 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.cc
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -1,43 +1,28 @@
 #include <iostream>
-#include <queue>
 using namespace std;
+
+int GCD(int a, int b) {
+	while (b != 0) {
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+
 	int t, a, b;
 	cin >> t;
 
 	while (--t >= 0) {
 		cin >> a >> b;
-
-		queue<int> q;
-
-		int temp = a;
-		for (int i = 2; i * i <= a; ++i) {
-			while (temp % i == 0) {
-				temp /= i;
-				q.push(i);
-			}
-		}
-		q.push(temp);
-
-		queue<int> tempQ = q;
-		while (!tempQ.empty()) {
-			if (b % tempQ.front() == 0)
-				b /= tempQ.front();
-			tempQ.pop();
-		}
-		q.push(b);
-
-		int result = 1;
-		while (!q.empty()) {
-			result *= q.front();
-			q.pop();
-		}
+		if (a < b) swap(a, b);
 		
+		int result = a * b / GCD(a, b);
 		cout << result << "\n";
-		
 	}
 
 	return 0;
