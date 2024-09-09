21dc89ecaf119bd37b42baf41f88538770accd88
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-09-09 11:47:49 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int arr[101]{};
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; ++i)
+		arr[i] = i;
+
+	while (m--) {
+		int i, j;
+		cin >> i >> j;
+		swap(arr[i], arr[j]);
+	}
+
+	for (int i = 1; i <= n; ++i)
+		cout << arr[i] << " ";
+
+	return 0;
+}
