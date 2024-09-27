7cf9f54f886efded17cef547ce834295b839d6ba
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2024-09-27 13:42:15 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	n = sqrt(n);
+	cout << n;
+	return 0;
+}
