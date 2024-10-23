adce21d2c2ba4a6ebb79618e6df220480323fc9f
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2024-10-23 15:19:29 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-pair<long long, long long> dp[90]{};
+long long dp[90]{};
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -10,15 +10,12 @@ int main() {
 	int n;
 	cin >> n;
 
-	dp[1] = { 0, 1 };
-	dp[2] = { 1, 0 };
-	dp[3] = { 1, 1 };
-	for (int i = 4; i <= n; ++i) {
-		dp[i].first = dp[i - 1].first + dp[i - 1].second;
-		dp[i].second = dp[i - 1].first;
-	}
-
-	cout << dp[n].first + dp[n].second;
+	dp[1] = 1;
+	dp[2] = 1;
+	for (int i = 3; i <= n; ++i)
+		dp[i] = dp[i - 1] + dp[i - 2];
+	
+	cout << dp[n];
 
 	return 0;
 }
