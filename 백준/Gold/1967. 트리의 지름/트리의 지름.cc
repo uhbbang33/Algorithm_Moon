45fa5434e6333e91c04d602f49e234c1c55ac557
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2024-11-08 15:07:01 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 3004 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -7,23 +7,22 @@ using namespace std;
 vector<pair<int, int>> v[MAX_N];
 bool visited[MAX_N]{};
 int maxResult = 0;
+int maxNode = 0;
 
-void DFS(int curNode, int curCost) {
+void DFS(int curNode, int curDist) {
+	if (visited[curNode])
+		return;
+	visited[curNode] = true;
+
+	if (maxResult < curDist) {
+		maxResult = curDist;
+		maxNode = curNode;
+	}
+
 	for (int i = 0; i < v[curNode].size(); ++i) {
 		int nextNode = v[curNode][i].first;
-		int nextCost = curCost + v[curNode][i].second;
-
-		if (visited[nextNode])
-			continue;
-
-		visited[nextNode] = true;
-
-		if (v[nextNode].size() == 1)
-			maxResult = max(maxResult, nextCost);
-		else 
-			DFS(nextNode, nextCost);
-		
-		visited[nextNode] = false;
+		int nextCost = curDist + v[curNode][i].second;
+		DFS(nextNode, nextCost);
 	}
 }
 
@@ -35,18 +34,19 @@ int main() {
 	cin >> n;
 
 	for (int i = 0; i < n - 1; ++i) {
-		int f, c, cost;
-		cin >> f >> c >> cost;
+		int f, c, dist;
+		cin >> f >> c >> dist;
 
-		v[f].push_back({ c, cost });
-		v[c].push_back({ f, cost });
+		v[f].push_back({ c, dist });
+		v[c].push_back({ f, dist });
 	}
 
-	for (int i = 1; i <= n; ++i)
-		if (v[i].size() == 1) {
-			visited[i] = true;
-			DFS(i, 0);
-		}
+	DFS(1, 0);
+	for (int i = 0; i <= n; ++i)
+		visited[i] = false;
+	maxResult = 0;
+
+	DFS(maxNode, 0);
 
 	cout << maxResult;
 	
