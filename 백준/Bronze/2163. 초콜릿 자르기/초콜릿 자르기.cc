7db05d09d82817967629d4d3966a5ac34e6a7fb1
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2024-11-23 07:37:44 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -1,34 +1,13 @@
 #include <iostream>
-#include <queue>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	priority_queue<int> pq;
-	int result = 0;
-
 	int n, m;
 	cin >> n >> m;
-	pq.push(n * m);
-
-	while (pq.top() != 1) {
-		int temp = pq.top();
-		pq.pop();
-
-		if (temp % 2 == 1) {
-			++result;
-		}
-
-		temp /= 2;
-		pq.push(temp);
-		pq.push(temp);
-
-		++result;
-	}
-	
-	cout << result;
+	cout << n * m - 1;
 
 	return 0;
 }
