29e1b7d972f80190a8db4ae6336f4ef44b4eccdf
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2024-12-06 16:22:46 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -10,39 +10,30 @@ void Func(int y, int x, int size) {
 	if (size > 2) {
 		int s = size / 2;
 
-		if (y + s > r && x + s > c) {
-			Func(y, x, s);
-			return;
-		}
-		else
-			num += s * s;
+		for (int i = 0; i < 4; ++i) {
+			int ny = y + (i / 2) * s;
+			int nx = x + (i % 2) * s;
 
-		if (y + s > r && x + s + s > c) {
-			Func(y, x + s, s);
-			return;
+			if (ny <= r && r < ny + s
+				&& nx <= c && c < nx + s) {
+				Func(ny, nx, s);
+				return;
+			}
+			else
+				num += s * s;
 		}
-		else
-			num += s * s;
-
-		if (y + s + s > r && x + s > c) {
-			Func(y + s, x, s);
-			return;
-		}
-		else
-			num += s * s;
-
-		if (x + s + s > c && y + s + s > r)
-			Func(y + s, x + s, s);
 	}
 	else {
-		if (y == r && x == c)
-			cout << num;
 		if (y == r && x + 1 == c)
-			cout << num + 1;
-		if (y + 1 == r && x == c)
-			cout << num + 2;
-		if (y + 1 == r && x + 1 == c)
-			cout << num + 3;
+			++num;
+		if (y + 1 == r) {
+			if (x == c)
+				num += 2;
+			if (x + 1 == c)
+				num += 3;
+		}
+
+		cout << num;
 	}
 }
 
