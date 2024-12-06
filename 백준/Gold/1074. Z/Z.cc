ff01e3e6f8695b5ea6ceeb5d04bbede5cc6113cc
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2024-12-06 16:14:30 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+#define Z_MIN_SIZE 4
+
+int n, r, c;
+int num = 0;
+
+void Func(int y, int x, int size) {
+	if (size > 2) {
+		int s = size / 2;
+
+		if (y + s > r && x + s > c) {
+			Func(y, x, s);
+			return;
+		}
+		else
+			num += s * s;
+
+		if (y + s > r && x + s + s > c) {
+			Func(y, x + s, s);
+			return;
+		}
+		else
+			num += s * s;
+
+		if (y + s + s > r && x + s > c) {
+			Func(y + s, x, s);
+			return;
+		}
+		else
+			num += s * s;
+
+		if (x + s + s > c && y + s + s > r)
+			Func(y + s, x + s, s);
+	}
+	else {
+		if (y == r && x == c)
+			cout << num;
+		if (y == r && x + 1 == c)
+			cout << num + 1;
+		if (y + 1 == r && x == c)
+			cout << num + 2;
+		if (y + 1 == r && x + 1 == c)
+			cout << num + 3;
+
+		num += 4;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> r >> c;
+
+	Func(0, 0, pow(2, n));
+
+	return 0;
+}
