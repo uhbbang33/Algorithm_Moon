604f6ebbd64a19d5c62c230eb4c1354bd1223ee3
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2024-12-20 20:35:17 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b;
+	while (1) {
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+			break;
+
+		if (a > b) cout << "Yes" << "\n";
+		else cout << "No" << "\n";
+	}
+
+	return 0;
+}
