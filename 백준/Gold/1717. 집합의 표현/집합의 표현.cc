d560028c16d24c5c12ec054563c940fa31f7752f
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-01-10 19:52:22 +0900

    [Gold V] Title: 집합의 표현, Time: 32 ms, Memory: 6320 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int parent[1000001]{};
+
+int GetParent(int num) {
+	if (parent[num] == num)
+		return num;
+
+	parent[num] = GetParent(parent[num]);
+
+	return parent[num];
+}
+
+void UnionParent(int a, int b) {
+	a = GetParent(a);
+	b = GetParent(b);
+
+	if (a > b)
+		parent[a] = b;
+	else
+		parent[b] = a;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 0; i <= n; ++i)
+		parent[i] = i;
+
+	for (int i = 0; i < m; ++i) {
+		bool isUnionOper;
+		int a, b;
+		cin >> isUnionOper >> a >> b;
+
+		if (isUnionOper) {
+			if (GetParent(a) == GetParent(b))
+				cout << "YES\n";
+			else
+				cout << "NO\n";
+		}
+		else {
+			if (a == b)
+				continue;
+
+			UnionParent(a, b);
+		}
+	}
+
+	return 0;
+}
