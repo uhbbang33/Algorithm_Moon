ed7f05ad8549748f8bb6e8aa99eec3ecbc80ba23
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-02-22 23:56:16 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0; i < n; ++i) cin >> k >> k;
+	
+	cout << "비와이";
+
+	return 0;
+}
