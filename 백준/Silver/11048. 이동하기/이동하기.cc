1803e7c2b567367dec8daf08f3ccbb67cc7eebd9
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-03-14 08:28:58 +0900

    [Silver II] Title: 이동하기, Time: 64 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001][1001]{};
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; ++i) 
+		for (int j = 1; j <= m; ++j) {
+			int candy;
+			cin >> candy;
+
+			dp[i][j] = max(dp[i - 1][j - 1], max(dp[i - 1][j], dp[i][j - 1]));
+			dp[i][j] += candy;
+		}
+
+	cout << dp[n][m];
+
+	return 0;
+}
