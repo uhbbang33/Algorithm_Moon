f72e94747c90bfeefff02c4c7372d840cccafbe0
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-03-15 14:01:02 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	
+	if (n) cout << "Leading the Way to the Future";
+	else cout << "YONSEI";
+
+	return 0;
+}
