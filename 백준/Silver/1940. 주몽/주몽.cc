4c6f0ab465c11d406f66a21b424c38cfd4c7d4e7
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-04-21 11:48:22 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
@@ -11,26 +10,25 @@ int main() {
 
 	int n, m;
 	cin >> n >> m;
-	
-	//vector<int> v(n);
-	for (int i = 0; i < n; ++i) 
+
+	for (int i = 0; i < n; ++i)
 		cin >> v[i];
 
-	sort(v, v+n);
+	sort(v, v + n);
 
 	int result = 0;
-	int left= 0, right = n - 1;
+	int left = 0, right = n - 1;
 	while (left < right) {
 		int sum = v[left] + v[right];
-		
+
 		if (sum == m) {
 			++result;
 			++left;
 			--right;
 		}
-		else if (sum < m) 
+		else if (sum < m)
 			++left;
-		else if (sum > m) 
+		else if (sum > m)
 			--right;
 	}
 
