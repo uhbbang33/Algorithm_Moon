0b87fb866004fafe598ddc5e4c6398ce3a374c6e
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-04-25 11:44:15 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string s;
+	cin >> s;
+
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::"<< s<< "::fan:\n";
+	cout << ":fan::fan::fan:\n";
+
+	return 0;
+}
