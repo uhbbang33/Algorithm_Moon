b31d4bd2928dc0e949846da74354ee787ce5c790
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-04-28 20:39:44 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	if (n % 2 == 0)
+		cout << "SK";
+	else
+		cout << "CY";
+
+
+	return 0;
+}
