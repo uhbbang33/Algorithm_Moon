45274c50cb721eb800e23038b6b9225173ce6a18
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-04-29 13:49:32 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -16,12 +16,13 @@ int main() {
 	arr[2] = 2;
 	arr[3] = 4;
 
+	for (int i = 4; i <= 1000000; ++i)
+		arr[i] = (arr[i - 1] + arr[i - 2] + arr[i - 3]) % MOD;
+
+
 	while (t--) {
 		int n;
 		cin >> n;
-
-		for (int i = 4; i <= n; ++i) 
-			arr[i] = (arr[i - 1] + arr[i - 2] + arr[i - 3]) % MOD;
 
 		cout << arr[n] % MOD << "\n";
 	}
