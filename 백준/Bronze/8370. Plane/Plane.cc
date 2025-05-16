43c48d3dce032f8bbd8edb003e118dad6cb969db
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-05-16 15:55:14 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n1, k1, n2, k2;
+	cin >> n1 >> k1 >> n2 >> k2;
+
+	cout << n1 * k1 + n2 * k2;
+
+	return 0;
+}
