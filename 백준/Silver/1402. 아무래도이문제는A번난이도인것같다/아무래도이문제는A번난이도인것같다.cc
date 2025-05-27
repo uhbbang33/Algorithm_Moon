92911e08823fb2933b896446a8d71372b137d0ce
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-05-27 13:05:26 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+
+		cout << "yes" << "\n";
+	}
+
+	return 0;
+}
