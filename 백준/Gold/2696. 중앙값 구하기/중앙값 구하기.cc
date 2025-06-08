f818f04dcfcd7bc5f0e9d03a62ef469cf78c7dfd
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-06-08 14:34:27 +0900

    [Gold II] Title: 중앙값 구하기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int m;
+		cin >> m;
+
+		priority_queue<int> pq;
+		priority_queue<int, vector<int>, greater<int>> pqGreater;
+
+		vector<int> result;
+
+		for (int i = 1; i <= m; ++i) {
+			int a;
+			cin >> a;
+
+			pq.push(a);
+
+			while (!pqGreater.empty() && !pq.empty()
+				&& pq.top() > pqGreater.top()) {
+				pqGreater.push(pq.top());
+				pq.pop();
+			}
+
+			while (pq.size() <= pqGreater.size()) {
+				pq.push(pqGreater.top());
+				pqGreater.pop();
+			}
+
+			while (pq.size() > pqGreater.size() + 1) {
+				pqGreater.push(pq.top());
+				pq.pop();
+			}
+
+			if (i % 2 == 1)
+				result.push_back(pq.top());
+		}
+
+		cout << result.size() << "\n";
+		for (int i = 0; i < result.size(); ++i) {
+			cout << result[i] << " ";
+
+			if ((i+1) % 10 == 0)
+				cout << "\n";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
