958baf1e88c45f712d128a8a7e75d0115c479e8b
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-06-13 11:09:56 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m, k;
+	cin >> n >> m >> k;
+
+	cout << k / m << " " << k % m;
+
+	return 0;
+}
