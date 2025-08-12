652ffe5eb5c9d2df1ed95bad366d10e0c3663344
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-08-12 13:50:08 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 248 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1000001]{};
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) 
+		cin >> arr[i];
+
+	sort(arr, arr + n, greater<>());
+
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << "\n";
+
+	return 0;
+}
