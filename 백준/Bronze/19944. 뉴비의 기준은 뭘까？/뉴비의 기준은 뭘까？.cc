227e61034d41d50495c20e04417ef7834e4db8cb
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-08-14 14:46:27 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	if (m == 1 || m == 2)
+		cout << "NEWBIE!";
+	else if (m <= n)
+		cout << "OLDBIE!";
+	else
+		cout << "TLE!";
+
+	return 0;
+}
