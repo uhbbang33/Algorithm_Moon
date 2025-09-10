078f11b65e7bc1abac5868788851e5fd0e23f892
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-09-10 13:46:30 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string s;
+	cin >> s;
+	
+	if (s == "NLCS")
+		cout << "North London Collegiate School";
+	if (s == "BHA")
+		cout << "Branksome Hall Asia";
+	if (s == "KIS")
+		cout << "Korea International School";
+	if (s == "SJA")
+		cout << "St. Johnsbury Academy";
+
+	return 0;
+}
