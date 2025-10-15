3d9722e28438033980b548ffca9ade012bf838ed
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-10-15 12:47:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 120.76 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <unordered_set>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+
+    for(int a : arr){
+        if(!answer.empty() && answer.back() == a)
+            continue;
+        
+        answer.push_back(a);
+    }
+    
+    return answer;
+}
