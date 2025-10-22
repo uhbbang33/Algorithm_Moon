7ac1d8066d5093351def6e9e93e90ff0ede7057c
문원정 Moon Won Jeong <55686424+uhbbang33@users.noreply.github.com>
2025-10-22 15:29:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 104.23 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    map<string, int> m;
+    
+    for(int i = 0; i < completion.size(); ++i)
+        ++m[completion[i]];
+    
+    for(int i =0 ;i < participant.size(); ++i){
+        if(--m[participant[i]] < 0){
+            answer = participant[i];
+            break;
+        }
+    }
+    
+    
+    return answer;
+}
